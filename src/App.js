5321e21703ffbf73970af0d2e4beb97c204cdcdb
adwillioh <adwillioh@gmail.com>
2022-11-15 14:17:43 -0500

    Changes in message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div><h1>Greetings from Static React Web App-1"</h1></h1>Hello {value}</div>;
+  return <div><h1>Greetings from Static React Web App-1</h1><h1>Hello {value}</h1></div>;
 }
 
 export default App;
